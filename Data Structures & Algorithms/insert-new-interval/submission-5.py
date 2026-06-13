e2808aee9a2ac07877fa72f74cd02324ed8d685c
HinanoOno <107801598+HinanoOno@users.noreply.github.com>
2026-06-13 23:13:08 +0900

    Add: insert-new-interval - submission-5

---
+++ Data Structures & Algorithms/insert-new-interval/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort()
+        n = len(intervals)
+        stack = [intervals[0]]
+        print(intervals)
+        for s,e in intervals[1:]:
+            if s>stack[-1][1]:
+                stack.append([s,e])
+            else:
+            
+                stack[-1] = [min(stack[-1][0],s),max(stack[-1][1],e)]
+        return stack
+
+        
