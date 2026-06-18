5645788f0904490ff03283f5051a2f38864643f1
HinanoOno <107801598+HinanoOno@users.noreply.github.com>
2026-06-18 18:22:28 +0900

    Add: time-based-key-value-store - submission-15

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-15.py
@@ -0,0 +1,29 @@
+class TimeMap:
+
+    def __init__(self):
+        self.data = {}
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.data:
+            self.data[key].append((timestamp,value))
+        else:
+            self.data[key] = [(timestamp,value)  ]
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.data:
+            cand = self.data[key]
+            cand.sort()
+            if cand[0][0]>timestamp:
+                return ""
+            l,r = 0,len(cand)
+            while (r-l)>1:
+                mid = (l+r)//2
+                if cand[mid][0]<=timestamp:
+                    l = mid
+                else:
+                    r = mid
+            return cand[l][1]
+        else:
+            return ""
+        
